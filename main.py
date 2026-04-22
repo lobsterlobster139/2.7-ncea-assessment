d27f84a627af39524ab824acb05c7d79c3bc610a
PhosphorusCat <ametcu@icloud.com>
2026-04-22 12:41:02 +1200

    made program skeleton

---
+++ main.py
@@ -0,0 +1,27 @@
+def set_size():
+    pass
+
+def set_dough():
+    pass
+
+def set_crust():
+    pass
+
+def add_topping():
+    pass
+
+def add_pizza():
+    pass
+
+def add_drink():
+    pass
+
+def remove_item():
+    pass
+
+def add_to_order():
+    pass
+
+def main_menu():
+    pass
+
