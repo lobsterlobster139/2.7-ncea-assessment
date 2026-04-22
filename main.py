beb14af004f69c065150bc75b19905569c235f89
PhosphorusCat <ametcu@icloud.com>
2026-04-22 12:56:06 +1200

    main menu finished

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+def input_validation():
+    pass
+
 def set_size():
     pass
 
@@ -11,17 +14,41 @@ def add_topping():
     pass
 
 def add_pizza():
-    pass
+    print("adding pizza")
 
 def add_drink():
-    pass
+    print("adding drink")
 
 def remove_item():
-    pass
+    print("removing item")
 
 def add_to_order():
     pass
 
+def confirm_order():
+    print("confirming order")
+
 def main_menu():
-    pass
+    print("Hello! What would you like to add to your order?\nPlease type the letter corresponding to the action you would like to do.")
+    option = input("""
+a) Add a pizza
+b) Add a drink
+c) Remove an item from your order
+d) Confirm order
+""").lower()
+    if option == "a":
+        add_pizza()
+    elif option == "b":
+        add_drink()
+    elif option == "c":
+        remove_item()
+    elif option == "d":
+        confirm_order()
+    else:
+        print("Not a valid option. Please re-input your choice.")
+    
+
+main_menu()
+    
+
 
