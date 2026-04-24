f39e9f24f8b1096365158faa7490b71b2274d8a7
PhosphorusCat <ametcu@icloud.com>
2026-04-24 15:00:59 +1200

    add drink finished + order formatting finished

--- main.py
+++ main.py
@@ -36,22 +36,40 @@ def add_topping(current_pizza):
             break
         current_pizza['toppings'].append(choice)
 
+def calc_price(pizza):
+    small_price = 9.0
+    medium_price = 10.0
+    large_price = 12.0
+    if pizza['size'] == "Small":
+        pizza['price'] = small_price
+    elif pizza['size'] == "Medium":
+        pizza['price'] = medium_price
+    elif pizza['size'] == "Large":
+        pizza['price'] = large_price
+
+
+
 def add_pizza(order):
     pizza = {
         'type' : "pizza",
         'size' : "",
         'dough' : "",
         'crust' : "",
-        'toppings' : []
+        'toppings' : [],
+        'price' : 0.0,
     }
     pizza['size'] = set_size()
     pizza['dough'] = set_dough()
     pizza['crust'] = set_crust()
+    calc_price(pizza)
     add_topping(pizza)
     order.append(pizza)
 
 def add_drink(order):
-    print("adding drink")
+    options = ["Ginger beer", "Fanta", "Pepsi", "Sprite"]
+    question = "What drink would you like? Ginger beer, Fanta, Pepsi or Sprite? (Type the drink you want): "
+    choice = input_validation(options, question)
+    order.append({'type': "drink", 'name': choice, 'price' : 4.0})
 
 def remove_item(order):
     print("removing item")
@@ -68,29 +86,46 @@ def print_item(item):
         print(f" Toppings: ")
         for topping in item['toppings']:
             print(f"     {topping}")
+        print(f" Price: ${item['price']:.2f}\n")
+    else:
+        print(f"Can of {item['name']}")
+        print(f" Price: ${item['price']:.2f}\n")
+
+def print_total_price(order):
+    total_price = 0.0
+    for item in order:
+        total_price += item['price']
+    print(f"Total Price: ${total_price:.2f}")
+        
 
 def main_menu():
     order = []
     print("Hello! What would you like to add to your order?\nPlease type the letter corresponding to the action you would like to do.")
-    option = input("""
+    while True:
+        option = input("""
 a) Add a pizza
 b) Add a drink
 c) Remove an item from your order
 d) Confirm order
 """).lower()
-    if option == "a":
-        add_pizza(order)
-        print("CURRENT ORDER:\n")
-        for item in order:
-            print_item(item)
-    elif option == "b":
-        add_drink(order)
-    elif option == "c":
-        remove_item(order)
-    elif option == "d":
-        confirm_order(order)
-    else:
-        print("Not a valid option. Please re-input your choice.")
+        if option == "a":
+            add_pizza(order)
+            print("\nCURRENT ORDER:")
+            for item in order:
+                print_item(item)
+            print_total_price(order)
+        elif option == "b":
+            add_drink(order)
+            print("\nCURRENT ORDER:")
+            for item in order:
+                print_item(item)
+            print_total_price(order)
+        elif option == "c":
+            remove_item(order)
+        elif option == "d":
+            confirm_order(order)
+        else:
+            print("Not a valid option. Please re-input your choice.")
     
 
 main_menu()
